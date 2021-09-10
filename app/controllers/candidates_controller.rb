7ff1c97ecd942ec77c1d02233fd44c0fe3a887d8
ttnppedr <priest0204@gmail.com>
2021-09-10 16:14:02 +0800

    Refactor find by id

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,6 @@
 class CandidatesController < ApplicationController
+  before_action :find_candidate, only: [:edit, :update, :destroy, :vote]
+
   def index
     @candidates = Candidate.all
   end
@@ -18,12 +20,9 @@ class CandidatesController < ApplicationController
   end
 
   def edit
-    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def update
-    @candidate = Candidate.find_by(id: params[:id])
-
     if @candidate.update(candidate_params)
       redirect_to candidates_path, notice: "資料更新成功！"
     else
@@ -32,13 +31,11 @@ class CandidatesController < ApplicationController
   end
 
   def destroy
-    @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy if @candidate
     redirect_to candidates_path, notice: "候選人資料已刪除！"
   end
 
   def vote
-    @candidate = Candidate.find_by(id: params[:id])
     @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
     redirect_to candidates_path, notice: "完成投票！"
   end
@@ -46,5 +43,9 @@ class CandidatesController < ApplicationController
   private
   def candidate_params
     params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
+
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
   end
 end
