a7e3b87414573476047829f81b93cda6c8fab21e
ttnppedr <priest0204@gmail.com>
2021-09-10 16:11:22 +0800

    Add counter cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_075614) do
+ActiveRecord::Schema.define(version: 2021_09_10_080737) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_10_075614) do
     t.integer "votes", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vote_logs_count"
   end
 
   create_table "vote_logs", force: :cascade do |t|
